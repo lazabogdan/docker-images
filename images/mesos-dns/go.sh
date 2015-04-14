324cbd88fea90612486f6fe34f1a1760245c8c2a
Laza Bogdan <laza.bogdan@macro-net.ro>
2015-04-14 16:21:29 +0300

    added mesos-dns

---
+++ images/mesos-dns/go.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+export GOPATH=/build
+go get github.com/miekg/dns
+go get github.com/mesosphere/mesos-dns
+cd $GOPATH/src/github.com/mesosphere/mesos-dns
+go build -o mesos-dns
+cp mesos-dns /
